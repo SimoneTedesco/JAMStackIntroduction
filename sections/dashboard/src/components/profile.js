500cf11b5049fe01ff128d67998edb91cf16c060
� Simone <simonetedesco.dev@gmail.com>
2020-12-11 23:35:28 +0100

    setup login and authentication through netlify

--- sections/dashboard/src/components/profile.js
+++ sections/dashboard/src/components/profile.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useIdentityContext } from "react-netlify-identity";
 
-const Profile = () => {
+const Profile = ({ showModal }) => {
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+  const name =
+    identity &&
+    identity.user &&
+    identity.user.user_metadata &&
+    identity.user.user_metadata.full_name;
+
   return (
-    <div className="dashboard-header">
-      <nav>
-        <Link to="/dashboard/secret" activeClassName="active">
-          Secret stuff
-        </Link>
-        <Link to="/dashboard/base" activeClassName="active">
-          See your base
-        </Link>
-      </nav>
-      <span>TODO SHOW LOGIN STATUS</span>
-    </div>
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="/dashboard/secret" activeClassName="active">
+            Secret stuff
+          </Link>
+          <Link to="/dashboard/base" activeClassName="active">
+            See your base
+          </Link>
+        </nav>
+        <span>
+          Logged in as {name}. <button onClick={showModal}>log out</button>
+        </span>
+      </div>
+    )
   );
 };
 
--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Router } from "@reach/router";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
 import Profile from "../components/profile";
 import RouteBase from "../components/route-base";
 import RouteSecret from "../components/route-secret";
 import RouteLogin from "../components/route-login";
+import "react-netlify-identity-widget/styles.css";
+import { navigate } from "gatsby";
 
-const Dashboard = () => {
+const Dashboard = ({ location }) => {
+  const [isVisible, setVisibility] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\?/)) {
+      navigate("/dashboard/login", { replace: true });
+    }
+  }, [location.pathname]);
+
+  const showModal = () => setVisibility(true);
+
   return (
     <Layout>
-      <Profile />
+      <Profile showModal={showModal} />
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
-      <p>TODO: create dashboard</p>
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setVisibility(false)}
+      />
     </Layout>
   );
 };
